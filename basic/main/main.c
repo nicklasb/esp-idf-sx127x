8da10c291accefb4ee8b26fa1f30f57a927891a0
Nicklas Börjesson <nicklasb@gmail.com>
2022-12-14 00:10:58 +0100

    Implement sdp send side

--- basic/main/main.c
+++ basic/main/main.c
@@ -1,21 +1,29 @@
+#include "sdkconfig.h"
 #include <stdio.h>
 #include <inttypes.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "esp_log.h"
 #include "lora.h"
+#include "freertos/timers.h"
 
 #if CONFIG_SENDER
 void task_tx(void *pvParameters)
 {
 	ESP_LOGI(pcTaskGetName(NULL), "Start");
 	uint8_t buf[256]; // Maximum Payload size of SX1276/77/78/79 is 255
+	uint64_t starttime;
+	int tx_count = 0;
 	while(1) {
-		TickType_t nowTick = xTaskGetTickCount();
-		int send_len = sprintf((char *)buf,"Hello World!! %"PRIu32, nowTick);
+		//TickType_t nowTick = xTaskGetTickCount();
+		tx_count++;
+		int send_len = sprintf((char *)buf,"Hello World!! %i", tx_count);
+		starttime = esp_timer_get_time();
+		ESP_LOGI(pcTaskGetName(NULL), "Sending %d bytes...", send_len);
 		lora_send_packet(buf, send_len);
-		ESP_LOGI(pcTaskGetName(NULL), "%d byte packet sent...", send_len);
-		vTaskDelay(pdMS_TO_TICKS(5000));
+		ESP_LOGI(pcTaskGetName(NULL), "%d byte packet sent...speed %f byte/s", send_len, 
+		(float)send_len/((float)(esp_timer_get_time()-starttime))*1000000);
+		vTaskDelay(pdMS_TO_TICKS(2000));
 	} // end while
 }
 #endif // CONFIG_SENDER
@@ -28,8 +36,12 @@ void task_rx(void *pvParameters)
 	while(1) {
 		lora_receive(); // put into receive mode
 		if (lora_received()) {
-			int receive_len = lora_receive_packet(buf, sizeof(buf));
-			ESP_LOGI(pcTaskGetName(NULL), "%d byte packet received:[%.*s]", receive_len, receive_len, buf);
+			int receive_len = lora_receive_packet(&buf, sizeof(buf));
+			if (receive_len > 4) {
+			ESP_LOGI(pcTaskGetName(NULL), "%d byte packet received:[%.*s], RSSI %i", 
+			receive_len, receive_len -4, &(buf[4]), lora_packet_rssi());
+			}
+
 		} // end if
 		vTaskDelay(1); // Avoid WatchDog alerts
 	} // end while
@@ -61,22 +73,22 @@ void app_main()
 	ESP_LOGI(pcTaskGetName(NULL), "Frequency is 915MHz");
 	lora_set_frequency(915e6); // 915MHz
 #elif CONFIG_OTHER
-	ESP_LOGI(pcTaskGetName(NULL), "Frequency is %dMHz", CONFIG_OTHER_FREQUENCY);
+	ESP_LOGI(pcTaskGetName(NULL), "Frequency is %d MHz", CONFIG_OTHER_FREQUENCY);
 	long frequency = CONFIG_OTHER_FREQUENCY * 1000000;
 	lora_set_frequency(frequency);
 #endif
-
-	lora_enable_crc();
+	lora_disable_crc();
+	//lora_enable_crc();
 
 	int cr = 1;
 	int bw = 7;
 	int sf = 7;
-#if CONFIF_ADVANCED
-	cr = CONFIG_CODING_RATE
+#if CONFIG_ADVANCED
+	cr = CONFIG_CODING_RATE;
 	bw = CONFIG_BANDWIDTH;
 	sf = CONFIG_SF_RATE;
 #endif
-
+	lora_set_tx_power(17);
 	lora_set_coding_rate(cr);
 	//lora_set_coding_rate(CONFIG_CODING_RATE);
 	//cr = lora_get_coding_rate();
